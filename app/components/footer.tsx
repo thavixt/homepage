c5dfb7b8e326058f83161b0b97a180f094e75525
thavixt <komlosi.peti@gmail.com>
2025-06-05 12:06:24 +0200

    chore: readme, update footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,10 @@
 export function Footer() {
   return (
     <footer className="text-sm font-light text-center">
-      made by <a className="link" href="http://github.com/thavixt" target="_blank" rel="noopener noreferrer">@thavixt on GitHub</a>
+      <div className="bg-gray-900 inline-block px-2 py-1 rounded-md text-white opacity-35 hover:opacity-100 transition-opacity">
+        {/* <span className="text-gray-200">Made with ❤️ by </span> */}
+        <a className="link" href="http://github.com/thavixt/homepage" target="_blank" rel="noopener noreferrer">made by @thavixt</a>
+      </div>
     </footer>
   )
 }
