2e7c7f616f74eb9244260c2d0bcb4a49dab75548
thavixt <komlosi.peti@gmail.com>
2026-01-07 14:18:29 +0100

    fix alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,21 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig((): UserConfig => {
   // const apiProxyTarget = "http://localhost:3000/api";
   // const apiProxyTarget = "https://personal.komlosidev.net/api";
   return {
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        "~": resolve(__dirname, "app"),
+      },
+    },
+    build: {
+      sourcemap: false,
+    },
     // server: {
     //   proxy: {
     //     '^api/.*': {
